ded0a6fc6d02687ca6b936aec611e8fcd745e630
emulator.chen <emulator.chen@gmail.com>
2026-02-27 22:06:09 +0800

    Init update for LC-52Z5T

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # homebridge-sharptv
-homebridge plugin for a sharp tv and other sharp products.
+Homebridge Plugin for SHarp TV's by using TCP/IP (Not RS232 version).
 
-Models tested:
+The reason I update this plugin is because most of the plugins supported SharpTV either useless(never tested) or not supported by my model.
 
--LC-##LE830U
+RS232( or IR control) can do more than TCP commands. But I would like to use TCP for stable controls.
 
--BD-HP35U
+---
+
+## Features
+
+- Custom command and statusParam
+- Support all Sharp TV that is accept TCP command
+- Bugfix for TCP connection does not close properly
+  
+As it's still pretty far to have a fully support by using DyanmicPlatform, this plugin is aimed to support TV control by presenting a switch On/Off.
+
+You can customize the On/Off switch to have multiple switches work together. 
+
+For example, you can have second switch named "PS5" and says **on value** to switch to input 6 and off presents input 5. So you can actually switch to input 6 by telling Siri to turn on PS5.
+
+---
+
+## Known limitation
+- Maximum 15 switches to the same TV
+  
+As my TV is pretty old(No android builtin), the maximum concurrent TCP connections accpeted by Sharp TV is around 15. And the homebrige will create 1 TCP connections for each switch. So the maximum amount of same switch will be approximately 15.
+
+If you find that Siri complains there's no response from the device, you can check the connections established to the same TV may already exceed. Reduce switch amount and restart homebridge will solve the issue.
+
+---
+
+## Models tested: 
+- LC-52Z5T
--- index.js
+++ index.js
@@ -1,145 +1,147 @@
 var Service;
 var Characteristic;
 
+//Currently homebridge does not support ES6 module
 var net = require('net');
-
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-
   homebridge.registerAccessory("homebridge-sharptv", "SharpTV", SharpTVAccessory);
+  return SharpTVAccessory;//A trick for test purpose as we don't want to expose the class
 }
 
-function SharpTVAccessory(log, config) {
-  this.log = log;
-  this.service = 'Switch';
-
-  this.name		       = config['name'];
-  this.onCommand	   = config['on'];
-  this.offCommand	   = config['off'];
-  this.stateCommand	 = config['state'];
-  this.onValue		   = config['on_value'];
-  this.onValue	   	 = this.onValue.trim();
-  this.exactMatch	   = config['exact_match'] || true;
-  this.host          = config['host'];
-  this.port          = config['port'];
-}
-
-SharpTVAccessory.prototype.matchesString = function(match) {
-  if(this.exactMatch) {
-    return (match === this.onValue);
+const SOCKET_TIME_OUT = 3*1000;
+class SharpTVAccessory {
+  constructor(log, config) {
+    this.log = log;
+    this.service = 'Switch';
+    this.userName = config['username'];
+    this.password = config['password'];
+    this.name = config['name'];
+    this.onCommand = config['on'];
+    this.offCommand = config['off'];
+    this.stateCommand = config['state'];
+    this.onValue = config['on_value'];
+    this.onValue = this.onValue.trim();
+    this.exactMatch = config['exact_match'] && true;
+    this.host = config['host'];
+    this.port = config['port'];
+    this.state = null; //value for passing through get/set state
+    this.accessory = this;
   }
-  else {
-    return (match.indexOf(this.onValue) > -1);
-  }
-}
-
-SharpTVAccessory.prototype.setState = function(powerOn, callback) {
-  var accessory = this;
-  var state = powerOn ? 'on' : 'off';
-  var prop = state + 'Command';
-  var command = accessory[prop];
-  var host = this.host;
-  var port = this.port;
-  var error;
-
-  var client = new net.Socket();
-  client.setTimeout(10000, function(err){
-      accessory.log('Timed out connecting to ' + host + ':' + port);
-  });
-  client.connect(port, host, function() {
-      client.write(command + '\r\n');
-  });
-
-  client.on('data', function(data) {
-      client.end();
-  });
-
-  client.on('close', function() {
-     if(!error){
-         accessory.log('Set ' + accessory.name + ' to ' + state);
-         callback(null);
-     }
-  });
-
-  client.on('error', function (err) {
-    accessory.log('Error: ' + err);
-    error = err;
-    client.destroy();
-    callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
-  });
-
-  client.on('timeout', function () {
-    error = "timedout";
-    client.destroy();
-    callback(new Error('Error setting ' + accessory.name + ' to ' + state + ' -timedout'));
-  });
-}
-
-SharpTVAccessory.prototype.getState = function(callback) {
-  var accessory = this;
-  var command = accessory['stateCommand'];
-  var host = this.host;
-  var port = this.port;
-  var state = "0";
-  var error;
-
-  var client = new net.Socket();
-  client.setTimeout(1000, function(err){
-      accessory.log('Timed out connecting to ' + host + ':' + port);
-
-  });
-  client.connect(port, host, function() {
-      client.write(command + '\r\n');
-  });
-
-  client.on('data', function(data) {
-      var input = data.toString('utf-8').trim();
-      if(input !== "ERR") {
-          if(input[0] === "1") {
-              state = "1";
-              client.end();
-          } else {
-              state = "0";
-              client.end();
-          }
-      }
-  });
-
-  client.on('close', function() {
-    //accessory.log('State of ' + accessory.name + ' is: ' + state);
-    if(!error) {
-        callback(null, accessory.matchesString("" + state));
+  matchesString(match) {
+    if (this.exactMatch) {
+      this.log('exactMatchResult:' + (match === this.onValue));
+      return (match === this.onValue);
     }
-  });
+    else {
+      this.log('NonExactMatchResult:('+this.onValue+','+match+')' + (this.onValue.indexOf(match) > -1));
+      return (this.onValue.indexOf(match) > -1);
+    }
+  }
+  setState(powerOn, callback) {
+    this.state = powerOn ? 'on' : 'off';
+    var command = this.accessory[this.state + 'Command'];
+    this.accessory.log('Going setState' + command);
+    this.sendCommand(command, callback);
+  }
+  getState(callback) {
+    this.state = "0";
+    var command = this.accessory['stateCommand'];
+    this.sendCommand(command, callback);
+  }
+  sendCommand(command, callback) {
+    const conn = new net.Socket();
+    const self = this;
+    command = this.userName + "\r" + this.password + "\r" + command;
+    conn.setTimeout(SOCKET_TIME_OUT, function (err) {
+      self.accessory.log('TVConnection connecting to ' + self.host + ':' + self.port + "  timedout");
+      this.error = "timedout";
+      this.destroy();
+      callback(new Error('Error having command at ' + self.accessory.name + ' to ' + self.state + ' -timedout'));
+    });
 
-  client.on('error', function (err) {
-    accessory.log('Error: ' + err);
-    client.destroy();
-  });
+    conn.connect(this.port, this.host, function () {
+      self.accessory.log('Send command(local-' + conn.localPort +'):' + command);
+      this.write(command + '\r');
+    });
 
-  client.on('timeout', function () {
-      error = "timedout";
-      client.destroy();
-      callback(new Error('Error setting ' + accessory.name + ' to ' + state + ' -timedout'));
-  });
-}
+    conn.on('data', function (data) {
+      var input = data.toString('utf-8').trim();
+      self.accessory.log('Response result:"' + input + '"');
+      if (input !== "ERR") {
+        const PASSWORD_STR = "Password:\r\n";
+        if (input === "") {
+          self.accessory.log('Ignore empty response');
+        }
+        else if ("0123456789OK".indexOf(input[0])) {
+          self.accessory.state = "1";
+          this.end();
+        } else if (input.trim().startsWith(PASSWORD_STR)) {
+          let res = input.substring(PASSWORD_STR.length).trim();
+          switch (res) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "OK":
+              self.accessory.log('GetState set State 1 By P1q$');
+              self.state = "1";
+              conn.end();
+              self.accessory.log('Destroy socket');
+              break;
+            case "ERR":
+              self.accessory.log('Received error code:'+res);
+              conn.end();
+              break;
+            default:
+              self.accessory.log("GetPasswordStateNonMatch:'"+res+"'");
+          }
+        }
+        else {
+          self.accessory.log('GetState set State 0T');
+          self.state = "0";
+        }
+      }
+    });
 
-SharpTVAccessory.prototype.getServices = function() {
-  var informationService = new Service.AccessoryInformation();
-  var switchService = new Service.Switch(this.name);
+    conn.on('end', ()=> {
+      self.accessory.log('TVConnection end:' + self.state);
+      conn.destroy();
+    })
 
-  informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'SharpTV Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'SharpTV Model')
-  .setCharacteristic(Characteristic.SerialNumber, 'SharpTV Serial Number');
+    conn.on('close', function () {
+      this.destroy();
+      if (!this.error) {
+        self.accessory.log('TVConnection Close:' + self.state);
+        callback(null, self.accessory.matchesString("" + self.state));
+      }
+    });
 
-  var characteristic = switchService.getCharacteristic(Characteristic.On)
-  .on('set', this.setState.bind(this));
+    conn.on('error', function (err) {
+      self.accessory.log('TVConnection Error: ' + err);
+      this.error = err;
+      this.destroy();
+    });
+  }
+  getServices() {
+    var informationService = new Service.AccessoryInformation();
+    var switchService = new Service.Switch(this.name);
 
-  if (this.stateCommand) {
-    characteristic.on('get', this.getState.bind(this))
-  };
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, 'Sharp')
+      .setCharacteristic(Characteristic.Model, 'SharpTV')
+      .setCharacteristic(Characteristic.SerialNumber, 'Serial Number');
 
-  return [switchService];
+    var characteristic = switchService.getCharacteristic(Characteristic.On)
+      .on('set', this.setState.bind(this));
+      characteristic.on('get', this.getState.bind(this));
+    return [switchService];
+  }
 }
