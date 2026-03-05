08216f0e8e77cc0cae15b0b320e4e82d1931612a
emulator.chen <emulator.chen@gmail.com>
2026-03-05 15:59:33 +0800

    Init update fix status issue

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class SharpTVAccessory {
     this.service = 'Switch';
     this.userName = config['username'];
     this.password = config['password'];
-    this.name = config['name'];
+    this.name = config['name'] || SharpTV;
     this.onCommand = config['on'];
     this.offCommand = config['off'];
     this.stateCommand = config['state'];
@@ -43,22 +43,29 @@ class SharpTVAccessory {
     this.state = powerOn ? 'on' : 'off';
     var command = this.accessory[this.state + 'Command'];
     this.accessory.log('Going setState' + command);
-    this.sendCommand(command, callback);
+    this.sendCommand(command, function(err) { callback(err); });
   }
   getState(callback) {
-    this.state = "0";
     var command = this.accessory['stateCommand'];
     this.sendCommand(command, callback);
   }
   sendCommand(command, callback) {
     const conn = new net.Socket();
     const self = this;
+    let done = false;
+    let localState = "0";
+    function finish(err, value) {
+      if (!done) {
+        done = true;
+        callback(err, value);
+      }
+    }
     command = this.userName + "\r" + this.password + "\r" + command;
-    conn.setTimeout(SOCKET_TIME_OUT, function (err) {
+    conn.setTimeout(SOCKET_TIME_OUT, function () {
       self.accessory.log('TVConnection connecting to ' + self.host + ':' + self.port + "  timedout");
       this.error = "timedout";
       this.destroy();
-      callback(new Error('Error having command at ' + self.accessory.name + ' to ' + self.state + ' -timedout'));
+      finish(null, false);
     });
 
     conn.connect(this.port, this.host, function () {
@@ -74,13 +81,18 @@ class SharpTVAccessory {
         if (input === "") {
           self.accessory.log('Ignore empty response');
         }
-        else if ("0123456789OK".indexOf(input[0])) {
-          self.accessory.state = "1";
+        else if ("0123456789OK".includes(input[0])) {
+          localState = "123456789OK".includes(input[0]) ? "1" : "0";
           this.end();
-        } else if (input.trim().startsWith(PASSWORD_STR)) {
+        } else if (input.startsWith(PASSWORD_STR)) {
           let res = input.substring(PASSWORD_STR.length).trim();
           switch (res) {
             case "0":
+                self.accessory.log('GetState set State 0 By P1q$');
+                localState = "0";
+                conn.end();
+                self.accessory.log('Destroy socket');
+                break;
             case "1":
             case "2":
             case "3":
@@ -92,7 +104,7 @@ class SharpTVAccessory {
             case "9":
             case "OK":
               self.accessory.log('GetState set State 1 By P1q$');
-              self.state = "1";
+              localState = "1";
               conn.end();
               self.accessory.log('Destroy socket');
               break;
@@ -105,22 +117,21 @@ class SharpTVAccessory {
           }
         }
         else {
-          self.accessory.log('GetState set State 0T');
-          self.state = "0";
+          self.accessory.log('Ignore unrecognised response:' + input);
         }
       }
     });
 
     conn.on('end', ()=> {
-      self.accessory.log('TVConnection end:' + self.state);
+      self.accessory.log('TVConnection end:' + localState);
       conn.destroy();
     })
 
     conn.on('close', function () {
       this.destroy();
       if (!this.error) {
-        self.accessory.log('TVConnection Close:' + self.state);
-        callback(null, self.accessory.matchesString("" + self.state));
+        self.accessory.log('TVConnection Close:' + localState);
+        finish(null, self.accessory.matchesString(localState));
       }
     });
 
@@ -128,6 +139,7 @@ class SharpTVAccessory {
       self.accessory.log('TVConnection Error: ' + err);
       this.error = err;
       this.destroy();
+      finish(null, false);
     });
   }
   getServices() {
